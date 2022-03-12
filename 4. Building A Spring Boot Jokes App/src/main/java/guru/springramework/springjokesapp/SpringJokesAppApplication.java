27c23522e2b3769bd8877c862d986414eafc9ad9
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-12 16:36:06 +0100

    Understanding Spring XML Configuration. closes #12.

--- 4. Building A Spring Boot Jokes App/src/main/java/guru/springramework/springjokesapp/SpringJokesAppApplication.java
+++ 4. Building A Spring Boot Jokes App/src/main/java/guru/springramework/springjokesapp/SpringJokesAppApplication.java
@@ -2,9 +2,11 @@ package guru.springramework.springjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class SpringJokesAppApplication {
+@ImportResource("classpath:chuck-config.xml")
+ public class SpringJokesAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringJokesAppApplication.class, args);
--- 4. Building A Spring Boot Jokes App/src/main/java/guru/springramework/springjokesapp/config/ChuckConfiguration.java
+++ 4. Building A Spring Boot Jokes App/src/main/java/guru/springramework/springjokesapp/config/ChuckConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChuckConfiguration {
 
-    @Bean
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes() {
         return new ChuckNorrisQuotes();
     }
