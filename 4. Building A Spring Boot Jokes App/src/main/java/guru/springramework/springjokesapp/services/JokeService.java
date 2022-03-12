906cec1b79f531ff9446dda7c4e9fd5a3b153422
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-12 14:27:33 +0100

    Creating the Spring Service Layer. closes #3.

---
+++ 4. Building A Spring Boot Jokes App/src/main/java/guru/springramework/springjokesapp/services/JokeService.java
@@ -0,0 +1,12 @@
+package guru.springramework.springjokesapp.services;
+
+/*
+PROJECT NAME : 4. Building A Spring Boot Jokes App
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/12/2022 2:23 PM
+*/
+
+public interface JokeService {
+    String getJoke();
+}
