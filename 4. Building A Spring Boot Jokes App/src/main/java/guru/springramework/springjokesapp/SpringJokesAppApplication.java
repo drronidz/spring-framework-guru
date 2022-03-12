558052021fd11a4ad6cd089bdac3a08f6f30c2bf
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-12 14:19:18 +0100

    Creating the Spring boot project. closes #2.

---
+++ 4. Building A Spring Boot Jokes App/src/main/java/guru/springramework/springjokesapp/SpringJokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springramework.springjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokesAppApplication.class, args);
+    }
+
+}
