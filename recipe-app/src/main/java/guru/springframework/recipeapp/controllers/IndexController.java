6eb93c96e7b8e37e511bf4bbdc4141433bbf6b96
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-13 21:54:31 +0100

    Setting Recipe project ...

---
+++ recipe-app/src/main/java/guru/springframework/recipeapp/controllers/IndexController.java
@@ -0,0 +1,20 @@
+package guru.springframework.recipeapp.controllers;
+
+/*
+PROJECT NAME : recipe-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/13/2022 9:45 PM
+*/
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
