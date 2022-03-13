8511d79941cbb47791fafe13a015b89c229d55cc
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-13 23:10:37 +0100

    Setting Many To Many relationship ...

---
+++ recipe-app/src/main/java/guru/springframework/recipeapp/models/Category.java
@@ -0,0 +1,46 @@
+package guru.springframework.recipeapp.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/*
+PROJECT NAME : recipe-app
+Module NAME: IntelliJ IDEA
+Author Name : @ DRRONIDZ
+DATE : 3/13/2022 10:54 PM
+*/
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
