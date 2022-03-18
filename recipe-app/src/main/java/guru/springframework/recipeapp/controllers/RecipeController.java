4ff8bfa4ee60e98131a3df06255497d1f4e03ae4
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-18 22:57:28 +0100

    Creating a new Recipe ...

--- recipe-app/src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
+++ recipe-app/src/main/java/guru/springframework/recipeapp/controllers/RecipeController.java
@@ -7,10 +7,13 @@ Author Name : @ DRRONIDZ
 DATE : 3/16/2022 9:25 PM
 */
 
+import guru.springframework.recipeapp.commands.RecipeCommand;
 import guru.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,7 +27,20 @@ public class RecipeController {
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
-
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String create(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/form";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
